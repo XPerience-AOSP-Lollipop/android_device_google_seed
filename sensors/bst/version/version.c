a4c26cc7ce6686b62a7438f11d791260606ebe14
Caner Aydın <punisher_caner@windowslive.com>
2016-11-15 22:45:51 +0300

    Remove unrequired date functions

--- sensors/bst/version/version.c
+++ sensors/bst/version/version.c
@@ -5,7 +5,6 @@
 
 static const char daemon_version_str[] = "3.0.37";
 static const char hal_version_str[] = "3.0.15";
-static const char date_str[] = "2016-11-04 19:09:28";
 const char* get_daemon_version(void)
 {
 	return daemon_version_str;
@@ -14,8 +13,3 @@ const char* get_hal_version(void)
 {
 	return hal_version_str;
 }
-const char* get_build_date(void)
-{
-	return date_str;
-}
-
